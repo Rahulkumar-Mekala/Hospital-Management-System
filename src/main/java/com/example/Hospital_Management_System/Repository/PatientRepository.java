fe3c9b8b7b8a0c8cbd8da5b0ac0311ad549caefc
Rahul Kumar Mekala <YOUR_GITHUB_EMAIL>
2026-07-23 17:39:31 +0530

    Update Hospital Management System

--- src/main/java/com/example/Hospital_Management_System/Repository/PatientRepository.java
+++ src/main/java/com/example/Hospital_Management_System/Repository/PatientRepository.java
@@ -10,16 +10,16 @@ import com.example.Hospital_Management_System.entity.Patient;
 
 
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
-	  boolean existsByEmail(String email);
-	    boolean existsByPatientCode(String patientCode);
-		Optional<Patient> findByPatientCode(String patientCode);
-		@Query(value = """
-		        SELECT patient_code
-		        FROM patient
-		        ORDER BY CAST(SUBSTRING(patient_code, 4) AS INTEGER) DESC
-		        LIMIT 1
-		        """, nativeQuery = true)
-		 String findLastPatientCode();
+    boolean existsByEmail(String email);
+    boolean existsByPatientCode(String patientCode);
+	Optional<Patient> findByPatientCode(String patientCode);
+     @Query(value = """
+            SELECT patient_code
+            FROM patient
+            ORDER BY CAST(SUBSTRING(patient_code, 4) AS INTEGER) DESC
+            LIMIT 1
+            """, nativeQuery = true)
+    String findLastPatientCode();
     
 
 	}
